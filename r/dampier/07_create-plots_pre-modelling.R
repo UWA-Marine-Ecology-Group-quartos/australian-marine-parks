cd41803e7906aa6c2e80c2cffe0defad44d6c842
claudehspencer <claudespencer111@gmail.com>
2024-09-25 07:57:00 +0800

    add plots

---
+++ r/dampier/07_create-plots_pre-modelling.R
@@ -0,0 +1,174 @@
+###
+# Project: NESP 4.20 - Marine Park Dashboard reporting
+# Data:    Marine Park monitoring data syntheses, oceanographic data, marine park boundary files
+# Task:    Create pre-modelling figures for marine park reporting
+# Author:  Claude Spencer
+# Date:    June 2024
+###
+
+# Table of contents
+# 1. Overall location plot (including State and Commonwealth Marine Parks)
+# 2. Sampling location plot
+# 3. Key Ecological Features
+# 4. Historical Sea Levels
+# 5. Bathymetry cross section
+
+# Clear your environment
+rm(list = ls())
+
+# Set the study name and marine park name (for folder structure)
+name <- "DampierAMP"
+park <- "dampier"
+
+# Load libraries
+library(tidyverse)
+library(sf)
+library(rnaturalearth)
+library(metR)
+library(patchwork)
+library(terra)
+library(tidyterra)
+library(ggnewscale)
+library(CheckEM)
+library(geosphere)
+
+# Load functions
+file.sources = list.files(pattern = "*.R", path = "functions/", full.names = T)
+sapply(file.sources, source, .GlobalEnv)
+
+# Set cropping extent - larger than most zoomed out plot
+e <- ext(116.7, 117.7,-20.919, -20)
+
+# Load necessary spatial files
+sf_use_s2(T)
+# Australian outline and state and commonwealth marine parks
+aus    <- st_read("data/south-west network/spatial/shapefiles/STE_2021_AUST_GDA2020.shp") %>%
+  st_make_valid()
+ausc <- st_crop(aus, e)
+
+# Load marine parks
+# aus_marine_parks <- st_read("data/spatial/shapefiles/Collaborative_Australian_Protected_Areas_Database_(CAPAD)_2022_-_Marine.shp")
+
+# All australian marine parks - for inset plotting
+aus_marine_parks <- st_read("data/south-west network/spatial/shapefiles/western-australia_marine-parks-all.shp")
+
+marine_parks <- st_read("data/south-west network/spatial/shapefiles/western-australia_marine-parks-all.shp") %>%
+  dplyr::filter(name %in% c("Dampier")) %>%
+  glimpse()
+
+# Australian Marine Parks only (for separate ggplot legends)
+marine_parks_amp <- marine_parks %>%
+  dplyr::filter(epbc %in% "Commonwealth")
+
+# State Marine Parks only (for separate ggplot legends)
+marine_parks_state <- marine_parks %>%
+  dplyr::filter(epbc %in% "State")
+
+# Terrestrial parks
+terrnp <- st_read("data/south-west network/spatial/shapefiles/Legislated_Lands_and_Waters_DBCA_011.shp") %>%  # Terrestrial reserves
+  dplyr::filter(leg_catego %in% c("Nature Reserve", "National Park"))
+plot(terrnp["leg_catego"])
+
+terr_fills <- scale_fill_manual(values = c("National Park" = "#c4cea6",          # Set the colours for terrestrial parks
+                                           "Nature Reserve" = "#e4d0bb"),
+                                name = "Terrestrial Parks")
+
+# Key Ecological Features
+# This shapefile has added columns in QGIS for hex colour code and abbreviated names
+kef <- st_read("data/south-west network/spatial/shapefiles/AU_DOEE_KEF_2015.shp") %>%
+  CheckEM::clean_names() %>%
+  st_make_valid() %>%
+  st_crop(e) %>%
+  arrange(desc(area_km2)) %>%
+  glimpse()
+unique(kef$abbrv) # None in Dampier
+
+# Coastal waters limit
+cwatr <- st_read("data/south-west network/spatial/shapefiles/amb_coastal_waters_limit.shp") %>%
+  st_make_valid() %>%
+  st_crop(e)
+
+# Bathymetry data
+bathy <- rast("data/south-west network/spatial/rasters/Australian_Bathymetry_and_Topography_2023_250m_MSL_cog.tif") %>%
+  crop(e) %>%
+  clamp(upper = 0, values = F)
+names(bathy) <- "Depth"
+plot(bathy)
+
+bathdf <- as.data.frame(bathy, xy = T)
+
+# Create marine park colours and fills (scale_fill_manual)
+# amp_fills <- amp_marine_park_fills(marine_parks)
+# state_fills <- state_marine_park_fills(marine_parks)
+# amp_cols <- amp_marine_park_cols(marine_parks)
+# state_cols <- state_marine_park_fills(marine_parks)
+
+# 1. Location overview plot
+# Set plot inputs
+plot_limits = c(116.779, 117.544, -20.738, -20.282) # Extent of the main plot
+study_limits = c(116.86, 117.4,-20.51, -20.3) # Extent of sampling
+annotation_labels = data.frame(x = c(117.1935), # Labels for annotation e.g. nearby towns
+                               y = c(-20.6287),
+                               label = c("Point Samson"))
+# Create plot
+location_plot(plot_limits,
+              study_limits,
+              annotation_labels)
+# Save plot
+ggsave(paste(paste0('plots/', park, '/spatial/', name) , 'broad-site-plot.png',
+             sep = "-"), dpi = 600, width = 8, height = 3, bg = "white")
+
+# 2. Site level overview - with sampling point locations
+metadata <- readRDS(paste0("data/", park, "/tidy/", name, "_metadata-bathymetry-derivatives.rds")) %>%
+  st_as_sf(coords = c("longitude_dd", "latitude_dd"), crs = 4326) %>%
+  glimpse()
+
+# Set plot inputs
+site_limits = c(116.779, 117.544, -20.738, -20.282) # For Dampier match it to the first plot
+# Create plot
+site_plot(site_limits, annotation_labels)
+# Save plot
+ggsave(filename = paste(paste0('plots/', park, '/spatial/', name) , 'sampling-locations.png',
+                        sep = "-"), plot = p2, units = "in", dpi = 600,
+       bg = "white",
+       width = 8, height = 4)
+
+# 3. Key Ecological Features
+# Create plot
+# kef_plot(plot_limits, annotation_labels)
+# Save plot
+# ggsave(filename = paste(paste0('plots/', park, '/spatial/', name) , 'key-ecological-features.png',
+#                         sep = "-"), units = "in", dpi = 600,
+#        bg = "white",
+#        width = 8, height = 6)
+
+# 4. Historical sea levels
+# Set coastline fills
+depth_fills <- scale_fill_manual(values = c("#f9ddb1","#ee9f27", "#dc6601"),
+                                 labels = c("9-10 Ka", "15-17 Ka", "20-30 Ka"),
+                                 name = "Coastline age")
+# Create plot
+sealevel_plot(plot_limits, annotation_labels)
+# Save plot
+ggsave(filename = paste(paste0('plots/', park, '/spatial/', name) , 'old-sea-levels.png',
+                        sep = "-"), units = "in", dpi = 600,
+       bg = "white",
+       width = 8, height = 5)
+
+# 5. Bathymetry cross sections
+# Create data
+bath_df1 <- dem_cross_section(116.7475, 116.9888, -20.6993, -20.2273, maxdist = 10)
+# Set plot inputs
+crosssection_labels = data.frame(x = c(-5, -10, -16), # Labels for annotation
+                                 y = c(100, 80, 10),
+                                 label = c("Burrup Peninsula", "Dolphin Island", "Legendre Island"))
+
+segment_offset <- 5 # Length of the segment
+label_offset <- segment_offset + 2 # Distance from end of segment to label
+# Create plot
+crosssection_plot(crosssection_labels, label_offset, segment_offset)
+# Save plot
+ggsave(filename = paste(paste0('plots/', park, '/spatial/', name) , 'bathymetry-cross-section.png',
+                        sep = "-"), units = "in", dpi = 600,
+       bg = "white",
+       width = 8, height = 4)
---
+++ r/dampier/09_create-plots_post-modelling_fish.R
@@ -0,0 +1,76 @@
+###
+# Project: NESP 4.20 - Marine Park Dashboard reporting
+# Data:    Fish data synthesis & habitat models derived from FSSgam
+# Task:    Create post-modelling fish figures for marine park reporting
+# Author:  Claude Spencer
+# Date:    June 2024
+###
+
+# Clear your environment
+rm(list = ls())
+
+# Set the study name
+name <- "GeographeAMP"
+park <- "geographe"
+
+# Load libraries
+library(tidyverse)
+library(terra)
+library(sf)
+library(ggplot2)
+library(ggnewscale)
+library(scales)
+library(viridis)
+library(patchwork)
+library(tidyterra)
+library(png)
+
+# Load functions
+file.sources = list.files(pattern = "*.R", path = "functions/", full.names = T)
+sapply(file.sources, source, .GlobalEnv)
+
+dat <- readRDS(paste0("output/model-output/", park, "/fish/",
+                      name, "_predicted-fish.RDS")) %>%
+  rast(crs = "epsg:4326")
+plot(dat)
+
+# Set cropping extent - larger than most zoomed out plot
+e <- ext(114.2, 115.8,-34.7, -33.1)
+
+# Load necessary spatial files
+sf_use_s2(F)                                                                    # Switch off spatial geometry for cropping
+# Australian outline and state and commonwealth marine parks
+marine_parks <- st_read("data/south-west network/spatial/shapefiles/western-australia_marine-parks-all.shp") %>%
+  dplyr::filter(name %in% c("Ngari Capes", "Geographe", "South-west Corner")) %>%
+  glimpse()
+plot(marine_parks["zone"])
+
+marine_parks_amp <- marine_parks %>%
+  dplyr::filter(epbc %in% "Commonwealth")
+marine_parks_state <- marine_parks %>%
+  dplyr::filter(epbc %in% "State")
+
+# Australian outline and state and commonwealth marine parks
+aus    <- st_read("data/south-west network/spatial/shapefiles/aus-shapefile-w-investigator-stokes.shp")
+ausc <- st_crop(aus, e)
+
+# Spatial predictions
+
+prediction_limits = c(115.0539, 115.5539, -33.64861, -33.35361)
+fishmetric_plot(prediction_limits)
+
+ggsave(paste0("plots/", park, "/fish/", name, "_individual-predictions.png"),
+       width = 9, height = 5, dpi = 300, units = "in", bg = "white")
+
+
+controldata_fish(year = 2014, amp_abbrv = "GMP", state_abbrv = "NCMP")
+
+controlplot_fish(data = park_dat.shallow, amp_abbrv = "GMP",
+                 state_abbrv = "NCMP", title = "Shallow (0 - 30 m)")
+ggsave(paste0("plots/geographe/fish/", name, "_shallow_control-plots.png"),
+       height = 7, width = 8, dpi = 300, units = "in", bg = "white")
+
+controlplot_fish(data = park_dat.meso, amp_abbrv = "GMP",
+                 state_abbrv = "NCMP", title = "Mesophotic (30 - 70 m)")
+ggsave(paste0("plots/geographe/fish/", name, "_mesophotic_control-plots.png"),
+       height = 7, width = 8, dpi = 300, units = "in", bg = "white")
