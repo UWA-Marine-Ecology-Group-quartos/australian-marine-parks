8a7541f9e057020db1b9b3c165a6a60115828a38
claudehspencer <claudespencer111@gmail.com>
2024-10-07 14:59:44 +0800

    working on quarto report

---
+++ r/geographe/02_create-report/02_spatial-layers.R
@@ -0,0 +1,265 @@
+###
+# Project: NESP 4.20 - Marine Park Dashboard reporting
+# Data:    Spatial covariates
+# Task:    Format spatial covariates, extract covariates for each sampling location
+# Author:  Claude Spencer
+# Date:    June 2024
+###
+
+# Clear the environment
+rm(list = ls())
+
+# Set the study name
+name <- "GeographeAMP"
+park <- "geographe"
+
+# Load libraries
+library(sf)
+library(terra)
+library(stars)
+library(starsExtra)
+library(tidyverse)
+library(tidyterra)
+library(patchwork)
+library(RNetCDF)
+
+# Set the extent of the study
+e <- ext(115.05, 115.558, -33.67, -33.349)
+
+# Load the bathymetry data (GA 250m resolution)
+bathy <- rast("data/south-west network/spatial/rasters/Australian_Bathymetry_and_Topography_2023_250m_MSL_cog.tif") %>%
+  crop(e) %>%
+  clamp(upper = 0, lower = -250, values = F) %>%
+  trim()
+plot(bathy)
+
+# Create terrain metrics (bathymetry derivatives)
+preds <- terrain(bathy, neighbors = 8,
+                 v = c("aspect", "roughness"),
+                 unit = "degrees")
+names(preds) <- c("geoscience_aspect", "geoscience_roughness")
+
+# Create detrended bathymetry
+zstar <- st_as_stars(bathy)
+detre <- detrend(zstar, parallel = 8)
+detre <- as(object = detre, Class = "SpatRaster")
+names(detre) <- c("geoscience_detrended", "lineartrend")
+
+# Join depth, terrain metrics and detrended bathymetry
+preds <- rast(list(bathy, preds, detre[[1]]))
+names(preds)[1] <- "geoscience_depth"
+
+# Save the bathymetry derivatives
+saveRDS(preds, file = paste0("data/", park, "/spatial/rasters/",
+                      name, "_bathymetry-derivatives.rds"))
+
+# Read in the metadata
+
+# metadata <- readRDS(paste0("data/tidy/",
+#                            name, "_metadata.rds")) %>%
+#   dplyr::mutate(longitude_dd = as.numeric(longitude_dd),
+#                 latitude_dd = as.numeric(latitude_dd)) %>%
+#   glimpse()
+
+metadata <- readRDS(paste0("data/", park, "/raw/", name, "_metadata.RDS")) %>%
+  dplyr::select(campaignid, sample, longitude_dd, latitude_dd, status) %>%
+  glimpse()
+
+# Convert metadata to a spatial file and check alignment with bathymetry
+metadata_sf <- st_as_sf(metadata, coords = c("longitude_dd", "latitude_dd"), crs = 4326)
+
+# Check that samples align with bathymetry derivatives
+plot(preds[[1]])
+plot(metadata_sf, add = T)
+
+# Extract bathymetry derivatives at each of the samples
+metadata.bathy.derivatives   <- cbind(metadata,
+                                      terra::extract(preds, metadata_sf)) %>%
+  filter_at(vars(geoscience_depth, geoscience_aspect, geoscience_roughness, geoscience_detrended),
+            all_vars(!is.na(.))) %>% # Removes samples missing bathymetry derivatives - check these!!
+  dplyr::select(-ID) %>%
+  glimpse()
+
+# Save the metadata bathymetry derivatives
+saveRDS(metadata.bathy.derivatives, paste0("data/", park, "/tidy/", name, "_metadata-bathymetry-derivatives.rds"))
+
+# Oceanography/Pressures
+# Sea surface temperature
+nc_sst <- open.nc(paste0("data/", park, "/spatial/oceanography/SST.nc"), write = TRUE)
+print.nc(nc_sst) # shows you all the file details
+time_nc <- var.get.nc(nc_sst, 'time')  # NC_CHAR time:units = "days since 1981-01-01 00:00:00" ;
+time_nc_sst <- utcal.nc("seconds since 1981-01-01 00:00:00", time_nc, type = "c")
+dates_sst <- as.Date(time_nc_sst)
+close.nc(nc_sst) # GDAL errors otherwise
+
+rast_sst <- rast(paste0("data/", park, "/spatial/oceanography/SST.nc"),
+                 subds = "sea_surface_temperature") %>%
+  crop(e) %>%
+  trim()
+plot(rast_sst)
+names(rast_sst) <- dates_sst
+time(rast_sst) <- dates_sst
+
+winter_sst_ts <- rast_sst[[names(rast_sst)[str_detect(names(rast_sst), "-06-|-07-|-08-")]]]
+
+for (month in unique(month(time(rast_sst)))) {
+  print(month)
+  monthly_rast <- subset(rast_sst, month(time(rast_sst)) == month) %>%
+    mean(na.rm = T) %>%
+    app(fun = function(i) {i - 273.15})
+  names(monthly_rast) <- month.abb[month]
+  if (month == 3) {
+    sst <- monthly_rast
+  }
+  else {
+    sst <- rast(list(sst, monthly_rast))
+  }
+}
+
+saveRDS(sst, paste0("data/", park, "/spatial/oceanography/", name, "_SST_raster.rds"))
+
+sst_tsdf <- terra::global(rast_sst, fun = "mean", na.rm = T) %>%
+  tibble::rownames_to_column() %>%
+  cbind(terra::global(rast_sst, fun = "sd", na.rm = T)) %>%
+  # dplyr::mutate(temp = mean - 273.15, # Convert kelvin to celsius
+  #               date = date(rowname)) %>%
+  tidyr::separate(rowname, into = c("year", "month", "day"), sep = "-") %>%
+  dplyr::group_by(year, month) %>%
+  summarise(sst = mean(mean, na.rm = T) - 273.15, # Convert kelvin to celsius
+            sd = mean(sd, na.rm = T)) %>%
+  ungroup() %>%
+  dplyr::mutate(season = case_when(month %in% c("03", "04", "05") ~ "Autumn",
+                                   month %in% c("06", "07", "08") ~ "Winter",
+                                   month %in% c("09", "10", "11") ~ "Spring",
+                                   month %in% c("12", "01", "02") ~ "Summer")) %>%
+  glimpse()
+
+saveRDS(sst_tsdf, paste0("data/", park, "/spatial/oceanography/", name, "_SST_time-series.rds"))
+
+# Sea Level Anomaly
+# nc_sla <- open.nc(paste0("data/geographe/spatial/oceanography/", name, "-SLA.nc"),
+#                   write = TRUE)
+nc_sla <- open.nc(paste0("data/", park, "/spatial/oceanography/SLA.nc"),
+                  write = TRUE)
+print.nc(nc_sla) # shows you all the file details
+time_nc <- var.get.nc(nc_sla, 'TIME')
+time_nc_sla <- utcal.nc("days since 1985-01-01 00:00:00 UTC", time_nc, type = "c")
+dates_sla <- as.Date(time_nc_sla)
+close.nc(nc_sla)
+
+rast_sla <- terra::rast(paste0("data/", park, "/spatial/oceanography/SLA.nc"),
+                        subds = "GSLA")
+time(rast_sla) <- dates_sla
+names(rast_sla) <- dates_sla
+plot(rast_sla)
+
+# sla <- mean(rast_sla, na.rm = T)
+# plot(sla)
+# saveRDS(sla, paste0("data/geographe/spatial/oceanography/", name, "_SLA_raster.rds"))
+
+for (month in unique(month(time(rast_sla)))) {
+  print(month)
+  monthly_rast <- subset(rast_sla, month(time(rast_sla)) == month) %>%
+    mean(na.rm = T)
+  names(monthly_rast) <- month.abb[month]
+  if (month == 1) {
+    sla <- monthly_rast
+  }
+  else {
+    sla <- rast(list(sla, monthly_rast))
+  }
+}
+
+saveRDS(sla, paste0("data/", park, "/spatial/oceanography/", name, "_SLA_raster.rds"))
+
+sla_tsdf <- terra::global(rast_sla, fun = "mean", na.rm = T) %>%
+  tibble::rownames_to_column() %>%
+  cbind(terra::global(rast_sla, fun = "sd", na.rm = T)) %>%
+  tidyr::separate(rowname, into = c("year", "month", "day"), sep = "-") %>%
+  dplyr::group_by(year, month) %>%
+  summarise(sla = mean(mean, na.rm = T),
+            sd = mean(sd, na.rm = T)) %>%
+  ungroup() %>%
+  dplyr::mutate(season = case_when(month %in% c("03", "04", "05") ~ "Autumn",
+                                   month %in% c("06", "07", "08") ~ "Winter",
+                                   month %in% c("09", "10", "11") ~ "Spring",
+                                   month %in% c("12", "01", "02") ~ "Summer")) %>%
+  glimpse()
+
+saveRDS(sla_tsdf, paste0("data/", park, "/spatial/oceanography/", name, "_SLA_time-series.rds"))
+
+# Degree Heating Weeks
+nc_dhw <- open.nc(paste0("data/", park, "/spatial/oceanography/DHW.nc"),
+                  write = TRUE)
+print.nc(nc_dhw) # shows you all the file details
+time_nc <- var.get.nc(nc_dhw, 'time')
+time_nc_dhw <- utcal.nc("seconds since 1970-01-01T00:00:00Z", time_nc, type = "c")
+dates_dhw <- as.Date(time_nc_dhw)
+close.nc(nc_dhw)
+
+rast_dhw <- terra::rast(paste0("data/", park, "/spatial/oceanography/DHW.nc"),
+                        subds = "CRW_DHW")
+time(rast_dhw) <- dates_dhw
+names(rast_dhw) <- dates_dhw
+plot(rast_dhw)
+
+dhw.2011 <- subset(rast_dhw, year(time(rast_dhw)) == 2011 & month(time(rast_dhw)) == 5) %>%
+  mean(na.rm = T)
+names(dhw.2011) <- "May 2011"
+plot(dhw.2011)
+dhw.2012 <- subset(rast_dhw, year(time(rast_dhw)) == 2012 & month(time(rast_dhw)) == 4) %>%
+  mean(na.rm = T)
+names(dhw.2012) <- "April 2012"
+plot(dhw.2012)
+
+dhw <- rast(list(dhw.2011, dhw.2012))
+plot(dhw)
+
+saveRDS(dhw, paste0("data/", park, "/spatial/oceanography/", name, "_DHW_raster.rds"))
+
+dhw_tsdf <- terra::global(rast_dhw, fun = "mean", na.rm = T) %>%
+  tibble::rownames_to_column() %>%
+  cbind(terra::global(rast_dhw, fun = "sd", na.rm = T)) %>%
+  tidyr::separate(rowname, into = c("year", "month", "day"), sep = "-") %>%
+  dplyr::group_by(year, month) %>%
+  summarise(dhw = mean(mean, na.rm = T),
+            sd = mean(sd, na.rm = T)) %>%
+  ungroup() %>%
+  dplyr::mutate(season = case_when(month %in% c("03", "04", "05") ~ "Autumn",
+                                   month %in% c("06", "07", "08") ~ "Winter",
+                                   month %in% c("09", "10", "11") ~ "Spring",
+                                   month %in% c("12", "01", "02") ~ "Summer")) %>%
+  glimpse()
+
+saveRDS(dhw_tsdf, paste0("data/", park, "/spatial/oceanography/", name, "_DHW_time-series.rds"))
+
+# Acifidication
+nc_acid <- open.nc(paste0("data/", park, "/spatial/oceanography/Acidification.nc"),
+                  write = TRUE)
+print.nc(nc_acid) # shows you all the file details
+time_nc <- var.get.nc(nc_acid, 'TIME')
+time_nc_acid <- utcal.nc("months since 1800-01-01 00:00:00", time_nc, type = "c")
+dates_acid <- as.Date(time_nc_acid)
+close.nc(nc_acid)
+
+rast_acid <- terra::rast(paste0("data/", park, "/spatial/oceanography/Acidification.nc"),
+                         subds = "pH_T")
+time(rast_acid) <- dates_acid
+names(rast_acid) <- dates_acid
+plot(rast_acid)
+
+acid_tsdf <- terra::global(rast_acid, fun = "mean", na.rm = T) %>%
+  tibble::rownames_to_column() %>%
+  cbind(terra::global(rast_acid, fun = "sd", na.rm = T)) %>%
+  tidyr::separate(rowname, into = c("year", "month", "day"), sep = "-") %>%
+  dplyr::group_by(year, month) %>%
+  summarise(acidification = mean(mean, na.rm = T),
+            sd = mean(sd, na.rm = T)) %>%
+  ungroup() %>%
+  dplyr::mutate(season = case_when(month %in% c("03", "04", "05") ~ "Autumn",
+                                   month %in% c("06", "07", "08") ~ "Winter",
+                                   month %in% c("09", "10", "11") ~ "Spring",
+                                   month %in% c("12", "01", "02") ~ "Summer")) %>%
+  glimpse()
+
+saveRDS(acid_tsdf, paste0("data/", park, "/spatial/oceanography/", name, "_Acidification_time-series.rds"))
---
+++ r/geographe/02_create-report/04_create-metrics_fish.R
@@ -0,0 +1,191 @@
+###
+# Project: NESP 4.20 - Marine Park Dashboard reporting
+# Data:    Fish data synthesis
+# Task:    Combine and format fish data for full subsets modelling
+# Author:  Claude Spencer
+# Date:    June 2024
+###
+
+# Clear the environment
+rm(list = ls())
+
+# Load necessary libraries
+library(CheckEM)
+library(tidyverse)
+library(sf)
+library(here)
+library(leaflet)
+library(googlesheets4)
+library(terra)
+library(tidyterra)
+
+# Set the study name
+name <- "GeographeAMP"
+park <- "geographe"
+
+metadata_bathy_derivatives <- readRDS(paste0("data/", park, "/tidy/", name, "_metadata-bathymetry-derivatives.rds")) %>%
+  clean_names() %>%
+  glimpse()
+
+metadata <- readRDS(paste0("data/", park, "/raw/", name, "_metadata.RDS"))
+
+# This is formatted habitat from 03_create-metrics_habitat
+benthos <- readRDS(paste0("data/", park, "/tidy/", name, "_benthos-count.RDS")) %>%
+  CheckEM::clean_names() %>%
+  dplyr::select(campaignid, sample, reef, total_pts) %>%
+  dplyr::mutate(reef = reef/total_pts) %>% # Model reef as proportion for fish prediction
+  glimpse()
+
+# Maturity data from WA sheet - should this just get included in the life history?
+maturity_mean <- CheckEM::maturity %>%
+  dplyr::filter(!marine_region %in% c("NW", "N")) %>% # Change here for each marine park
+  dplyr::group_by(family, genus, species, sex) %>%
+  dplyr::slice(which.min(l50_mm)) %>%
+  ungroup() %>%
+  dplyr::group_by(family, genus, species) %>%
+  dplyr::summarise(l50 = mean(l50_mm)) %>%
+  ungroup() %>%
+  glimpse()
+
+large_bodied_carnivores <- CheckEM::australia_life_history %>%
+  dplyr::filter(fb_trophic_level > 2.8) %>%
+  dplyr::filter(length_max_cm > 40) %>%
+  dplyr::filter(class %in% "Actinopterygii") %>%
+  dplyr::filter(!order %in% c("Anguilliformes", "Ophidiiformes", "Notacanthiformes","Tetraodontiformes","Syngnathiformes",
+                              "Synbranchiformes", "Stomiiformes", "Siluriformes", "Saccopharyngiformes", "Osmeriformes",
+                              "Osteoglossiformes", "Lophiiformes", "Lampriformes", "Beloniformes", "Zeiformes")) %>%
+  left_join(maturity_mean) %>%
+  dplyr::mutate(fb_length_at_maturity_mm = fb_length_at_maturity_cm * 10) %>%
+  dplyr::mutate(l50 = if_else(is.na(l50), fb_length_at_maturity_mm, l50)) %>%
+  dplyr::filter(!is.na(l50)) %>%
+  dplyr::select(family, genus, species, l50) %>%
+  glimpse()
+
+count <- readRDS(paste0("data/", park, "/raw/", name, "_complete_count.RDS")) %>%
+  dplyr::select(campaignid, sample, family, genus, species, count) %>%
+  dplyr::mutate(scientific_name = paste(family, genus, species, sep = " ")) %>%
+  glimpse()
+
+ta.sr <- count %>%
+  dplyr::select(-c(family, genus, species)) %>%
+  dplyr::group_by(campaignid, sample, scientific_name) %>%
+  pivot_wider(names_from = "scientific_name", values_from = count) %>%
+  dplyr::ungroup() %>%
+  dplyr::mutate(total_abundance = rowSums(.[, 3:(ncol(.))], na.rm = T),
+                species_richness = rowSums(.[, 3:(ncol(.))] > 0)) %>%
+  dplyr::select(campaignid, sample, total_abundance, species_richness) %>%
+  pivot_longer(cols = c("total_abundance", "species_richness"), names_to = "response", values_to = "number") %>%
+  glimpse() # Should be nsamps * 2 = 594
+
+cti <- CheckEM::create_cti(data = count) %>%
+  dplyr::rename(number = cti) %>%
+  dplyr::mutate(response = "cti") %>%
+  glimpse()
+
+tidy_maxn <- bind_rows(ta.sr, cti) %>%
+  dplyr::select(-c(log_count, w_sti, CTI)) %>%
+  dplyr::left_join(benthos) %>%
+  dplyr::left_join(metadata) %>% # To join samples without valid bathymetry derivatives
+  dplyr::left_join(metadata_bathy_derivatives) %>%
+  dplyr::filter(!is.na(reef), # GBR3-4 has no habitat
+                !is.na(geoscience_aspect)) %>% # Not valid values for modelling so will remove them now
+  glimpse()
+
+saveRDS(tidy_maxn, file = paste0("data/", park, "/tidy/", name, "_tidy-count.rds"))
+
+length <- readRDS(paste0("data/", park, "/raw/", name, "_complete_length.RDS")) %>%
+  dplyr::select(campaignid, sample, family, genus, species, length_mm, number) %>%
+  left_join(large_bodied_carnivores) %>%
+  dplyr::mutate(scientific_name = paste(genus, species, sep = " ")) %>%
+  glimpse()
+length(unique(length$sample))
+
+all_species <- length %>%
+  distinct(scientific_name) %>%
+  glimpse()
+
+test_species <- length %>%
+  dplyr::filter(!is.na(l50)) %>%
+  distinct(scientific_name) %>%
+  glimpse()
+
+metadata_length <- length %>%
+  distinct(campaignid, sample) %>%
+  glimpse()
+
+big_carn <- length %>%
+  dplyr::filter(length_mm > l50) %>%
+  dplyr::group_by(campaignid, sample) %>%
+  dplyr::summarise(number = sum(number)) %>%
+  ungroup() %>%
+  right_join(metadata_length) %>%
+  dplyr::mutate(number = ifelse(is.na(number), 0, number)) %>%
+  dplyr::mutate(response = "greater than Lm carnivores") %>%
+  left_join(benthos) %>%
+  dplyr::glimpse()
+# Check number of samples that are > 0
+nrow(filter(big_carn, number > 0))/nrow(big_carn)
+
+small_carn <- length %>%
+  dplyr::filter(length_mm < l50) %>%
+  dplyr::group_by(campaignid, sample) %>%
+  dplyr::summarise(number = sum(number)) %>%
+  ungroup() %>%
+  right_join(metadata_length) %>%
+  dplyr::mutate(number = ifelse(is.na(number), 0, number)) %>%
+  dplyr::mutate(response = "smaller than Lm carnivores") %>%
+  left_join(benthos) %>%
+  dplyr::glimpse()
+# Check number of samples that are > 0
+nrow(filter(small_carn, number > 0))/nrow(small_carn)
+
+big_snap <- length %>%
+  dplyr::filter(species %in% "auratus",
+                length_mm > l50) %>%
+  dplyr::group_by(campaignid, sample) %>%
+  dplyr::summarise(number = sum(number)) %>%
+  ungroup() %>%
+  right_join(metadata_length) %>%
+  dplyr::mutate(number = ifelse(is.na(number), 0, number)) %>%
+  dplyr::mutate(response = "greater than Lm Pink snapper") %>%
+  left_join(benthos) %>%
+  dplyr::glimpse()
+# Check number of samples that are > 0
+nrow(filter(big_snap, number > 0))/nrow(big_snap) # This won't run
+
+small_snap <- length %>%
+  dplyr::filter(species %in% "auratus",
+                length_mm < l50) %>%
+  dplyr::group_by(campaignid, sample) %>%
+  dplyr::summarise(number = sum(number)) %>%
+  ungroup() %>%
+  right_join(metadata_length) %>%
+  dplyr::mutate(number = ifelse(is.na(number), 0, number)) %>%
+  dplyr::mutate(response = "smaller than Lm Pink snapper") %>%
+  left_join(benthos) %>%
+  dplyr::glimpse()
+# Check number of samples that are > 0
+nrow(filter(small_snap, number > 0))/nrow(small_snap)
+
+tidy_length <- bind_rows(big_carn, small_carn, small_snap) %>% # Removed snapper - not enough non-zero data
+  dplyr::left_join(metadata) %>%
+  dplyr::left_join(metadata_bathy_derivatives) %>%
+  dplyr::filter(!is.na(reef), # GBR3-4 missing habitat
+                !is.na(geoscience_aspect)) %>% # Not valid values for modelling so will remove them now
+  glimpse()
+
+# Visualise spatial patterns
+preds <- readRDS(paste0("data/{", park, "/spatial/rasters/", name, "_bathymetry-derivatives.rds"))
+plot(preds)
+names(preds)
+
+ggplot() +
+  geom_spatraster(data = preds, aes(fill = geoscience_depth)) +
+  geom_point(data = tidy_length,
+             aes(x = longitude_dd, y = latitude_dd, size = number, colour = I(if_else(number == 0, "white", "darkblue"))),
+             show.legend = F) +
+  facet_wrap(~response) +
+  theme_classic() +
+  coord_sf()
+
+saveRDS(tidy_length, file = paste0("data/", park, "/tidy/", name, "_tidy-length.rds"))
