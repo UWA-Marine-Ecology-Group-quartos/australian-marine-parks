9d0a40cfc61b87f04fa071052ec7eb16db6983bb
claudehspencer <claudespencer111@gmail.com>
2024-11-27 10:42:29 +0800

    add shipping channel and spoil grouinds to location overview plot

--- r/dampier/07_create-plots_pre-modelling.R
+++ r/dampier/07_create-plots_pre-modelling.R
@@ -103,6 +103,19 @@ bathdf <- as.data.frame(bathy, xy = T)
 # amp_cols <- amp_marine_park_cols(marine_parks)
 # state_cols <- state_marine_park_fills(marine_parks)
 
+# Load Port Walcott shipping zones
+channel <- st_read("data/dampier/spatial/shapefiles/port-walcott_shipping-channel.shp") %>%
+  summarise(geometry = st_union(geometry)) %>%
+  dplyr::mutate(Infrastructure = "Shipping channel") %>%
+  glimpse()
+
+spoil <- st_read("data/dampier/spatial/shapefiles/port-walcott_spoil-grounds.shp") %>%
+  summarise(geometry = st_union(geometry)) %>%
+  dplyr::mutate(Infrastructure = "Spoil ground") %>%
+  glimpse()
+
+infrastructure <- bind_rows(channel, spoil)
+
 # 1. Location overview plot
 # Set plot inputs
 plot_limits = c(116.779, 117.544, -20.738, -20.282) # Extent of the main plot
@@ -110,13 +123,78 @@ study_limits = c(116.86, 117.4,-20.51, -20.3) # Extent of sampling
 annotation_labels = data.frame(x = c(117.1935), # Labels for annotation e.g. nearby towns
                                y = c(-20.6287),
                                label = c("Point Samson"))
+
+# Custom function for this - need to add new sf layers
+location_plot <- function(plot_limits, study_limits, annotation_labels) {
+  # 1. Location overview plot - includes parks zones and an aus inset
+  require(tidyverse)
+  require(tidyterra)
+  require(patchwork)
+  require(ggpattern)
+
+  p1 <- ggplot() +
+    geom_spatraster_contour_filled(data = bathy,
+                                   breaks = c(0, -30, -70, -200, - 700, -2000 , -4000, -6000),
+                                   colour = NA, show.legend = F) +
+    # scale_fill_grey(start = 1, end = 0.5, guide = "none") +
+    scale_fill_manual(values = c("#FFFFFF", "#EFEFEF", "#DEDEDE", "#CCCCCC", "#B6B6B6", "#9E9E9E", "#808080")) +
+    new_scale_fill() +
+    geom_spatraster_contour(data = bathy,
+                            breaks = c(-30, -70, -200, - 700, -2000 , -4000, -6000), colour = "white",
+                            alpha = 3/5, linewidth = 0.1, show.legend = F) +
+    geom_sf(data = ausc, fill = "seashell2", colour = "grey80", linewidth = 0.1) +
+    geom_sf(data = terrnp, aes(fill = leg_catego), colour = NA, alpha = 0.8) +
+    terr_fills +
+    new_scale_fill() +
+    geom_sf(data = marine_parks_state, aes(fill = zone), colour = NA, alpha = 0.4) +
+    scale_fill_manual(name = "State Marine Parks", guide = "legend",
+                      values = with(marine_parks_state, setNames(colour, zone))) +
+    new_scale_fill() +
+    geom_sf(data = marine_parks_amp, aes(fill = zone), colour = NA, alpha = 0.8) +
+    scale_fill_manual(name = "Australian Marine Parks", guide = "legend",
+                      values = with(marine_parks_amp, setNames(colour, zone))) +
+    new_scale_fill() +
+    geom_sf(data = cwatr, colour = "firebrick", alpha = 1, linewidth = 0.4, lineend = "round") +
+    geom_sf_pattern(data = infrastructure, aes(pattern = Infrastructure, pattern_fill = Infrastructure, colour = Infrastructure), alpha = 0.7,
+                    pattern_density = 0.8, pattern_size = 0.2, pattern_spacing = 0.005) +
+    scale_colour_manual(values = c("#4A4E69", "#C9ADA7")) +
+    scale_pattern_fill_manual(values = c("#4A4E69", "#C9ADA7")) +
+    scale_pattern_manual(values = c("stripe", "crosshatch")) +
+    # scale_fill_manual(values = c("Shipping channel" = "",
+    #                              "Spoil ground" = )) +
+    new_scale_fill() +
+    labs(x = NULL, y = NULL) +
+    annotate("text", x = annotation_labels$x,
+             y = annotation_labels$y,
+             label = annotation_labels$label, size = 1.65,
+             fontface = "italic") +
+    annotate("rect", xmin = study_limits[1], xmax = study_limits[2], ymin = study_limits[3], ymax = study_limits[4],
+             fill = NA, colour = "goldenrod2", linewidth = 0.4) +
+    coord_sf(xlim = c(plot_limits[1], plot_limits[2]), ylim = c(plot_limits[3], plot_limits[4]), crs = 4326) +
+    theme_minimal()
+
+  # inset map
+  p1.1 <- ggplot(data = aus) +
+    geom_sf(fill = "seashell1", colour = "grey90", linewidth = 0.05, alpha = 4/5) +
+    geom_sf(data = aus_marine_parks, alpha = 5/6, colour = "grey85", linewidth = 0.02) +
+    coord_sf(xlim = c(110, 125), ylim = c(-37, -13)) + # This is constant for all plots - its just a map of WA
+    annotate("rect", xmin = plot_limits[1], xmax = plot_limits[2], ymin = plot_limits[3], ymax = plot_limits[4],   # Change here
+             colour = "grey25", fill = "white", alpha = 1/5, linewidth = 0.2) +
+    theme_bw() +
+    theme(axis.text = element_blank(),
+          axis.ticks = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.border = element_rect(colour = "grey70"))
+
+  p1.1 + p1
+}
 # Create plot
 location_plot(plot_limits,
               study_limits,
               annotation_labels)
 # Save plot
 ggsave(paste(paste0('plots/', park, '/spatial/', name) , 'broad-site-plot.png',
-             sep = "-"), dpi = 600, width = 8, height = 3, bg = "white")
+             sep = "-"), dpi = 600, width = 8, height = 3.5, bg = "white")
 
 # 2. Site level overview - with sampling point locations
 metadata <- readRDS(paste0("data/", park, "/tidy/", name, "_metadata-bathymetry-derivatives.rds")) %>%
