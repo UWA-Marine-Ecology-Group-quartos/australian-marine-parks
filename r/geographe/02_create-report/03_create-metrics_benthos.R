b09ab325996fb31b43f5d1e960e27844e085e8aa
Henry Evans <h.j.evans@outlook.com>
2025-10-07 11:08:12 +0800

    Changed this script as old _benthos.RDS seems to have been in long format with different column names, but the current version is already in long format, and changes col names to be the same as what the old script set them to

--- r/geographe/02_create-report/03_create-metrics_benthos.R
+++ r/geographe/02_create-report/03_create-metrics_benthos.R
@@ -25,16 +25,10 @@ park <- "geographe"
 #   glimpse()
 
 benthos <- readRDS(paste0("data/", park, "/raw/", name, "_benthos.RDS")) %>%
-  dplyr::select(campaignid, sample, level_2, level_3, count) %>%
-  dplyr::mutate(habitat = case_when(level_2 %in% "Macroalgae" ~ "macroalgae",
-                                    level_2 %in% "Seagrasses" ~ "seagrasses",
-                                    level_3 %in% "Unconsolidated (soft)" ~ "sand",
-                                    level_3 %in% "Consolidated (hard)" ~ "rock",
-                                    level_3 %in% "Corals" ~ "sessile_invertebrates")) %>%
-  dplyr::select(campaignid, sample, habitat, count) %>%
-  pivot_wider(names_from = habitat, values_from = count, values_fill = 0) %>%
-  dplyr::mutate(total_pts = rowSums(.[3:ncol(.)], na.rm = T),
-                reef = macroalgae + rock + sessile_invertebrates) %>%
+  dplyr::select(campaignid, sample, macroalgae, seagrasses,
+                sand = unconsolidated, rock = consolidated,
+                sessile_invertebrates, total_pts = total_points_annotated) %>%
+  dplyr::mutate(reef = macroalgae + rock + sessile_invertebrates) %>%
   glimpse()
 
 length(unique(benthos$sample))
