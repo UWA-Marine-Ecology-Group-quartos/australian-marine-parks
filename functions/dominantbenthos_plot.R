c53aa41d7aae5cfa36af5a5e4a2fc92e84c29eaf
henry-j-e <h.j.evans@outlook.com>
2026-03-06 16:05:31 +0800

    Plotted dominant habitat categories and seperated scatterpies into years

--- functions/dominantbenthos_plot.R
+++ functions/dominantbenthos_plot.R
@@ -1,20 +1,20 @@
 dominantbenthos_plot <- function(prediction_limits) {
   ggplot() +
-    # geom_tile(data = pred_plot, aes(x = x, y = y, fill = p_inverts.alpha, alpha = p_inverts.fit)) +
-    # scale_alpha_continuous(range = c(0, 1), guide = "none", name = "Sessile invertebrates") +
-    # scale_fill_gradient(low = "white", high = "deeppink3", name = "Sessile invertebrates", na.value = "transparent") +
-    # new_scale_fill() +
-    # new_scale("alpha") +
+    geom_tile(data = pred_plot, aes(x = x, y = y, fill = p_inverts.alpha, alpha = p_inverts.fit)) +
+    scale_alpha_continuous(range = c(0, 1), guide = "none", name = "Sessile invertebrates") +
+    scale_fill_gradient(low = "white", high = "deeppink3", name = "Sessile invertebrates", na.value = "transparent") +
+    new_scale_fill() +
+    new_scale("alpha") +
     geom_tile(data = pred_plot, aes(x = x, y = y, fill = p_sand.alpha, alpha = p_sand.fit)) +
     scale_alpha_continuous(range = c(0, 1), guide = "none", name = "Sand") +
     scale_fill_gradient(low = "white", high = "wheat", name = "Sand", na.value = "transparent") +
     new_scale_fill() +
     new_scale("alpha") +
-    # geom_tile(data = pred_plot, aes(x = x, y = y, fill = p_rock.alpha, alpha = p_rock.fit)) +
-    # scale_alpha_continuous(range = c(0, 1), guide = "none", name = "Rock") +
-    # scale_fill_gradient(low = "white", high = "grey40", name = "Rock", na.value = "transparent") +
-    # new_scale_fill() +
-    # new_scale("alpha") +
+    geom_tile(data = pred_plot, aes(x = x, y = y, fill = p_rock.alpha, alpha = p_rock.fit)) +
+    scale_alpha_continuous(range = c(0, 1), guide = "none", name = "Rock") +
+    scale_fill_gradient(low = "white", high = "grey40", name = "Rock", na.value = "transparent") +
+    new_scale_fill() +
+    new_scale("alpha") +
     geom_tile(data = pred_plot, aes(x = x, y = y, fill = p_macro.alpha, alpha = p_macro.fit)) +
     scale_alpha_continuous(range = c(0, 1), guide = "none", name = "Macroalgae") +
     scale_fill_gradient(low = "white", high = "darkorange4", name = "Macroalgae", na.value = "transparent") +
--- r/geographe/02_create-report/08_create-plots_post-modelling_benthos.R
+++ r/geographe/02_create-report/08_create-plots_post-modelling_benthos.R
@@ -60,26 +60,36 @@ cwatr <- st_read("data/south-west network/spatial/shapefiles/amb_coastal_waters_
   st_crop(e) %>%
   st_transform(4326)
 
+# Load the bathymetry data (GA 250m resolution)
+bathy <- rast("data/south-west network/spatial/rasters/AusBathyTopo__Australia__2024_250m_MSL_cog.tif") %>%
+  crop(e) %>%
+  clamp(upper = 0, lower = -250, values = F) %>%
+  trim() %>%
+  as.data.frame(xy = T, na.rm = T) %>%
+  glimpse()
+
+names(bathy)[3] <- "Depth"
+
 # Read in the data (per year) ----
-pred.years <- c(2014L, 2024L)
+years <- c(2014L, 2024L)
 
-# hab_cols <- c(
-#   "Sand" = "wheat",
-#   "Macroalgae" = "darkgoldenrod4",
-#   "Seagrass" = "forestgreen",
-#   "Rock" = "grey40",
-#   "Sessile invertebrates" = "plum"
-# )
+hab_cols <- c(
+  "Sand" = "wheat",
+  "Macroalgae" = "darkgoldenrod4",
+  "Seagrass" = "forestgreen",
+  "Rock" = "grey40",
+  "Sessile invertebrates" = "plum"
+)
 
-for (pred_year in pred.years) {
+for (yr in years) {
 
-  message("Year: ", pred_year)
+  message("Year: ", yr)
 
   dat <- readRDS(paste0("output/model-output/", park, "/habitat/",
-                        name, "_predicted-habitat_", pred_year, ".rds"))
+                        name, "_predicted-habitat_", yr, ".rds"))
 
   pred_class <- as.data.frame(dat, xy = TRUE) %>%
-    dplyr::mutate(year = pred_year) %>%
+    dplyr::mutate(year = yr) %>%
     glimpse()
 
   # Normalise the inverse of standard error
@@ -87,6 +97,18 @@ for (pred_year in pred.years) {
 
   # Set the limits for the plot
   prediction_limits <- c(115.0539, 115.5539, -33.64861, -33.35361)
+
+  # ---- Dominant habitat categorical map (DISPLAY + SAVE) ----
+  p_cat <- categoricalhabitat_plot(prediction_limits)
+
+  print(p_cat)
+
+  ggsave(
+    filename = paste0("plots/", park, "/habitat/", name,
+                      "_predicted-habitat-categorical_", yr, ".png"),
+    plot = p_cat,
+    height = 6, width = 8, dpi = 600, units = "in", bg = "white"
+  )
 
   # ---- Dominant benthos ggplot (DISPLAY + SAVE) ----
   p_dom <- dominantbenthos_plot(prediction_limits) +
@@ -101,11 +123,11 @@ for (pred_year in pred.years) {
       legend.margin = margin(t = -0.1, unit = "cm")
     )
 
-  print(p_dom)  # <-- this makes it show up when looping
+  print(p_dom)
 
   ggsave(
     filename = paste0("plots/", park, "/habitat/", name,
-                      "_predicted-dominant-habitat_", pred_year, ".png"),
+                      "_predicted-dominant-habitat_", yr, ".png"),
     plot = p_dom,
     height = 6, width = 8, dpi = 600, units = "in", bg = "white"
   )
@@ -118,7 +140,6 @@ for (pred_year in pred.years) {
   )
 
   names(pred_rast) <- c("Sand", "Macroalgae", "Seagrasses", "Sessile Invertebrates", "Rock")
-  # plot(pred_rast)
 
   # ---- Individual benthos ggplot (DISPLAY + SAVE) ----
   p_ind <- individualbenthic_plot(prediction_limits)
@@ -127,14 +148,14 @@ for (pred_year in pred.years) {
 
   ggsave(
     filename = paste0("plots/", park, "/habitat/", name,
-                      "_predicted-individual-habitat_", pred_year, ".png"),
+                      "_predicted-individual-habitat_", yr, ".png"),
     plot = p_ind,
     height = 5.5, width = 8, dpi = 900, units = "in", bg = "white"
   )
 }
 
 # Create the data (makes a dataframe for each ecosystem depth contour)
-control_all <- purrr::map(pred.years, \(yy) {
+control_all <- purrr::map(years, \(yy) {
   dat_yy <- readRDS(paste0("output/model-output/", park, "/habitat/",
                            name, "_predicted-habitat_", yy, ".rds"))
   controldata_benthos(dat = dat_yy, year = yy, amp_abbrv = "GMP", state_abbrv = "NCMP")
@@ -163,7 +184,7 @@ ggsave(paste0("plots/", park, "/habitat/", name, "_mesophotic-control-plots.png"
 #        height = 9, width = 8, dpi = 300, units = "in")
 
 
-# Get depth for Scatterpie plot
+# ---- Scatterpie data prep ----
 
 # Set the extent of the study
 e <- ext(114.8, 116, -33.8, -33)
@@ -171,19 +192,20 @@ e <- ext(114.8, 116, -33.8, -33)
 # Load the bathymetry data (GA 250m resolution)
 bathy <- rast("data/south-west network/spatial/rasters/AusBathyTopo__Australia__2024_250m_MSL_cog.tif") %>%
   crop(e) %>%
-  clamp(upper = 0, lower = -250, values = F) %>%
+  clamp(upper = 0, lower = -250, values = FALSE) %>%
   trim() %>%
-  as.data.frame(xy = T, na.rm = T)
+  as.data.frame(xy = TRUE, na.rm = TRUE)
 
 names(bathy)[3] <- "Depth"
 
-# Scatterpies
+metadata_bathy_derivatives <- readRDS(
+  paste0("data/", park, "/tidy/", name, "_metadata-bathymetry-derivatives.rds")
+) %>%
+  clean_names()
 
-metadata_bathy_derivatives <- readRDS(paste0("data/", park, "/tidy/", name, "_metadata-bathymetry-derivatives.rds")) %>%
-  clean_names() %>%
-  glimpse()
-
-benthos <- readRDS(paste0("data/", park, "/tidy/", name, "_benthos-count_combined.RDS")) %>%
+benthos <- readRDS(
+  paste0("data/", park, "/tidy/", name, "_benthos-count_combined.RDS")
+) %>%
   dplyr::rename(
     Macroalgae = macroalgae,
     Seagrass = seagrasses,
@@ -191,13 +213,12 @@ benthos <- readRDS(paste0("data/", park, "/tidy/", name, "_benthos-count_combine
     Rock = rock,
     "Sessile invertebrates" = sessile_invertebrates
   ) %>%
-  left_join(metadata_bathy_derivatives) %>%
-  arrange(desc(Sand)) %>% # This plots sand underneath the other pies
-  glimpse()
+  left_join(metadata_bathy_derivatives, by = c("campaignid", "sample", "year", "status")) %>%
+  arrange(desc(Sand))
 
 hab_fills <- scale_fill_manual(
   name = "Habitat",
-  limits = c("Rock","Sessile invertebrates","Macroalgae","Seagrass","Sand"),
+  limits = c("Rock", "Sessile invertebrates", "Macroalgae", "Seagrass", "Sand"),
   values = c(
     "Rock" = "grey40",
     "Sessile invertebrates" = "plum",
@@ -206,7 +227,6 @@ hab_fills <- scale_fill_manual(
     "Sand" = "wheat"
   )
 )
-
 
 wampa_fills <- scale_fill_manual(values = c(
   # "Marine Management Area" = "#b7cfe1",
@@ -219,43 +239,39 @@ wampa_fills <- scale_fill_manual(values = c(
 ),
 name = "State Marine Parks")
 
-# depth colours
-depth_fills <- scale_fill_manual(values = c("#a7cfe0","#9acbec","#98c4f7",  # Shallow to deep
-                                            "#a3bbff", "#81a1fc"), guide = "none")
+depth_fills <- scale_fill_manual(
+  values = c("#a7cfe0", "#9acbec", "#98c4f7", "#a3bbff", "#81a1fc"),
+  guide = "none"
+)
 
-site_limits = c(115.0, 115.67,-33.3, -33.65)
+site_limits <- c(115.0, 115.67, -33.3, -33.65)
 
-ggplot() +
-  geom_contour_filled(data = bathy, aes(x, y, z = Depth, fill = after_stat(level)), color = "black",
-                      breaks = c(-30, -70, -200,-700, -2000, -4000), size = 0.1) +
-  depth_fills +
-  new_scale_fill() +
-  # geom_sf(data = marine_parks_amp, aes(fill = zone), colour = NA, show.legend = F,
-  #         linewidth = 0.75, alpha = 0.5) +
-  # scale_fill_manual(name = "Australian Marine Parks",
-  #                   values = with(marine_parks_amp, setNames(colour, zone))) +
-  geom_sf(data = ausc, fill = "seashell2", colour = "black", size = 0.1) +
-  geom_sf(data = wasanc ,fill = "#bfd054", alpha = 2/5, color = NA) +
-  wampa_fills +
-  labs(fill = "State Marine Parks") +
-  new_scale_fill() +
-  geom_sf(data = npz, fill = "#7bbc63",alpha = 2/5, color = NA) +
-  geom_sf(data = cwatr, colour = "firebrick", alpha = 4/5, size = 0.3) +
-  new_scale_fill() +
-  geom_scatterpie(data = benthos, aes(x = longitude_dd, y = latitude_dd),
-                  cols = c("Sand",
-                           "Sessile invertebrates",
-                           "Rock",
-                           "Macroalgae",
-                           "Seagrass"),
-                  colour = NA, pie_scale = 0.45) +
-  labs(x = "Longitude", y = "Latitude", fill = "Habitat") +
-  hab_fills +
-  coord_sf(xlim = c(site_limits[1], site_limits[2]), ylim = c(site_limits[3], site_limits[4]), crs = 4326) +
-  theme_minimal() +
-  theme(panel.background = element_rect(fill = "#b9d1d6", colour = NA),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank())
+years <- c(2014, 2024)
 
-ggsave(paste0("plots/", park, "/habitat/", name, "_scatterpie.png"),
-       height = 6, width = 10, dpi = 300, bg = "white")
+for (yr in years) {
+
+  message("Year: ", yr)
+
+  benthos_year <- benthos %>%
+    dplyr::filter(as.character(year) == as.character(yr)) %>%
+    dplyr::filter(
+      is.finite(longitude_dd),
+      is.finite(latitude_dd)
+    ) %>%
+    arrange(desc(Sand))
+
+  p_scatterpie <- scatterpie_plot(site_limits = site_limits, pie_scale = 0.45)
+
+  print(p_scatterpie)
+
+  ggsave(
+    filename = paste0(
+      "plots/", park, "/habitat/", name, "_scatterpie_", yr, ".png"
+    ),
+    plot = p_scatterpie,
+    height = 6,
+    width = 10,
+    dpi = 300,
+    bg = "white"
+  )
+}
